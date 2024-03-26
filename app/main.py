4db4c9533db310ad53557035dbb901ac76aa6f7f
Alizée Petrini <106526691+apliz13@users.noreply.github.com>
2024-03-26 16:40:20 +0100

    Co-authored-by: Thomas Genoud <th0my67@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ import pygame
 import numpy as np
 from time import time, mktime, strptime
 from numpy import array
-from pygame import display, image
 from json import load, dump
 from module import random_path
 
@@ -11,18 +10,29 @@ from module import random_path
 
 def main():
     pygame.init()
-    scrn = display.set_mode(Config.SCREEN_SIZE)
+    scrn = pygame.display.set_mode(Config.SCREEN_SIZE)
     img_1 = pygame.image.load(Config.image1)
     img_2 = pygame.image.load(Config.image2)
     pygame.display.set_caption('substitution images')
     imgarray_1 = pygame.surfarray.pixels2d(img_1)
     imgarray_2 = pygame.surfarray.pixels2d(img_2)
 
+    duration = mktime(strptime(Config.end_dt)) - mktime(strptime(Config.start_dt))
 
 
     # On récupère les coordonnées des pixels
-    with open(Config.coords_file) as f:
-        coords = load(f)
+    try:
+        with open(Config.coords_file) as f:
+            path = array(load(f))
+            
+    except:
+        path = random_path()
 
+    
+    for (x ,y) in path[current_pos:]:
+        imgarray_1[[x - 1:x + 1], y] = imgarray_2[[x - 1:x + 1], y]
+        imgarray_1[x, [ y - 1 :y + 1 ]] = imgarray_2[x, [y - 1:y + 1]]
+        if condition_afficher:
+            scrn.blit(img_1, (0, 0))
 
-random_path()
+    
--- app/module.py
+++ app/module.py
@@ -2,10 +2,12 @@ from numpy import array, random
 from config import Config
 from json import dump
 def random_path():
-    path = [None] * (Config.SCREEN_HEIGHT * Config.SCREEN_WIDTH)
-    for x in range(Config.SCREEN_WIDTH):
-        for y in range(Config.SCREEN_HEIGHT):
-            path[x * Config.SCREEN_HEIGHT + y] = (x, y)
+    path_heigth = Config.SCREEN_HEIGHT//3 + 1
+    path_width = Config.SCREEN_HEIGHT//3 + 1
+    path = [None] * (path_heigth * path_width)
+    for x in range(path_heigth):
+        for y in range(path_width):
+            path[x * Config.SCREEN_HEIGHT + y] = (x*3, y*3)
     random.shuffle(path)
     with open(Config.coords_file, 'w') as f:
         dump(path, f)
