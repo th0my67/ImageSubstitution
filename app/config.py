221ea3c6761824bfec16cca10f26ea4325ec9138
th0my67 <thomy672005@gmail.com>
2024-04-03 20:01:36 +0200

    Project Done
    Co-authored-by: Alizée Petrini <apliz13@users.noreply.github.com>

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 class Config:
+
+    #Nom de la fenêtre 
+    WINDOW_NAME = 'Transition 50 ans CSUD'
+
     # largeur de l'écran (et des images)
-    #SCREEN_WIDTH = 3840
-    SCREEN_WIDTH = 2000
-    #SCREEN_WIDTH = 640
+    SCREEN_WIDTH = 3840
+
     # hauteur de l'écran (et des images)
-    #SCREEN_HEIGHT = 2160
-    SCREEN_HEIGHT = 1000
-    #SCREEN_HEIGHT = 478
+    SCREEN_HEIGHT = 2160
     
     SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
     
@@ -15,17 +16,22 @@ class Config:
     
     # On fait la transition de puis l'image 1 vers l'image 2, situées dans le
     # dossier `data`
-    image1 = 'old.jpg'
-    image2 = 'new.jpg'
-    
+    image1 = 'route.jpg'
+    image2 = 'lac.jpg'
+
     # heure de départ
-    start_dt = "2024-03-07 08:00:00"
-    end_dt = "2024-03-07 0:24:00"
+    start_dt = "2024-04-03 20:00:00"
+    end_dt = "2024-04-03 20:01:00"
     
     # sauvegarde de l'ordre de transition des pixels
     coords_file = 'coords.json'
 
+    # Proportion du temps alloué aux points:
+    dot_time_proportion = 80
 
-    # Danger : Ne pas modifier ces valeurs
-    Cross_length = 3
-    Cross_height = 3
+
+    #Danger : ne pas toucher
+    cross_width = 3
+    cross_height = 3
+
+
--- app/main.py
+++ app/main.py
@@ -15,59 +15,113 @@ def main():
     imgarray_2 = cv2.imread(os.path.join("images",Config.image2))
 
     strt_time = mktime(strptime(Config.start_dt,"%Y-%m-%d %H:%M:%S"))
-    duration_constant = (mktime(strptime(Config.end_dt,"%Y-%m-%d %H:%M:%S")) - strt_time)/(Config.SCREEN_HEIGHT * Config.SCREEN_WIDTH / Config.Cross_length / Config.Cross_height)
+    total_duration = (mktime(strptime(Config.end_dt,"%Y-%m-%d %H:%M:%S")) - strt_time)
+
+    total_changes = Config.SCREEN_HEIGHT * Config.SCREEN_WIDTH / Config.cross_width / Config.cross_height
     uptade_frequency = 1/Config.desired_fps
 
+    cross_change_speed = total_changes / total_duration / Config.dot_time_proportion * 100
+    cross_entire_duration = total_duration * Config.dot_time_proportion / 100
 
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    dot_change_speed = total_changes / total_duration / (100 - Config.dot_time_proportion) * 100
 
 
+    cv2.namedWindow(Config.WINDOW_NAME, cv2.WINDOW_NORMAL)
 
-    cv2.imshow('image', imgarray_1)
-    cv2.waitKey(0)
-
+    cv2.imshow(Config.WINDOW_NAME, imgarray_1)
+    cv2.waitKey(1)
 
 
     try:
         with open(Config.coords_file) as f:
             path = array(json.load(f))
-            print('path loaded')
             
     except:
         path = random_path()
 
-    current_pos = 0 #int((time() - strt_time) * duration_constant)
+    #Attente du début
+    while time() < strt_time:
+        sleep(1)
+        cv2.imshow(Config.WINDOW_NAME, imgarray_1)
+        cv2.waitKey(1)
+
+    #Recommencer au bon endroit
+    cross_current_pos = int((time() - strt_time) * cross_change_speed)
     last_time = time()
-    for (x ,y) in path[current_pos:]:
+
+    #Rattrapage des croix
+    for pos_in_list ,(x ,y) in enumerate(path[:cross_current_pos]):
+
+        #Transition
+        imgarray_1[x - 1:x + 2, y] = imgarray_2[ x - 1:x + 2 , y]
+        imgarray_1[x, y - 1 :y + 2 ] = imgarray_2[x, y - 1 :y + 2]
+
+    #Transition des croix
+    for pos_in_list ,(x ,y) in enumerate(path[cross_current_pos:]):
+
+        #Transition
         imgarray_1[x - 1:x + 2, y] = imgarray_2[ x - 1:x + 2 , y]
         imgarray_1[x, y - 1 :y + 2 ] = imgarray_2[x, y - 1 :y + 2]
         
-        sleep(0.0001)
+        #Attente
+        while pos_in_list + cross_current_pos > (time()-strt_time) * cross_change_speed:
+            sleep(0.1)
+            if time() - last_time > uptade_frequency:
+                last_time = time()
+                cv2.imshow(Config.WINDOW_NAME, imgarray_1)
+                cv2.waitKey(1)
+        
+        #Affichage
         if time() - last_time > uptade_frequency:
             last_time = time()
-            cv2.imshow('image', imgarray_1)
+            cv2.imshow(Config.WINDOW_NAME, imgarray_1)
             cv2.waitKey(1)
 
+    #Transition des bords
     imgarray_1[0:3, : ] = imgarray_2[0:3, : ]
     imgarray_1[-3:, : ] = imgarray_2[-3:, : ]
     imgarray_1[:, 0:3] = imgarray_2[:, 0:3]
     imgarray_1[:, -3:] = imgarray_2[:, -3:]
 
-    for (x ,y) in path[current_pos:]:
+    #Recommencer au bon endroit
+    dot_current_pos = int((time() - strt_time - cross_entire_duration) * dot_change_speed)
+    if dot_current_pos < 0:
+        dot_current_pos = 0
+    
+    #Rattrapage des points
+    for pos_in_list, (x ,y) in enumerate(path[:dot_current_pos]):
+
+        #Transition
+        if x+1 < Config.SCREEN_HEIGHT and y-1 < Config.SCREEN_WIDTH:
+            imgarray_1[x - 2 : x, y + 1 : y + 3] = imgarray_2[ x - 2 : x, y + 1 : y + 3]
+
+
+    #Transition des points
+    for pos_in_list, (x ,y) in enumerate(path[dot_current_pos:]):
+
+        #Transition
         if x+1 < Config.SCREEN_HEIGHT and y-1 < Config.SCREEN_WIDTH:
             imgarray_1[x - 2 : x, y + 1 : y + 3] = imgarray_2[ x - 2 : x, y + 1 : y + 3]
         #else:
             #if x - 1 > 0 and y+2 < Config.SCREEN_WIDTH:
             #    imgarray_1[x - 1, y + 2] = imgarray_2[ x - 1, y + 2]
         
-        sleep(0.0001)
+        #Attente
+        while pos_in_list + dot_current_pos > (time() - strt_time - cross_entire_duration) * dot_change_speed :
+            sleep(0.1)
+            if time() - last_time > uptade_frequency:
+                last_time = time()
+                cv2.imshow(Config.WINDOW_NAME, imgarray_1)
+                cv2.waitKey(1)
+        
+        #Affichage
         if time() - last_time > uptade_frequency:
             last_time = time()
-            cv2.imshow('image', imgarray_1)
+            cv2.imshow(Config.WINDOW_NAME, imgarray_1)
             cv2.waitKey(1)
      
 
-    cv2.imshow('image', imgarray_1)
+    cv2.imshow(Config.WINDOW_NAME, imgarray_1)
     cv2.waitKey(0)
     
 main()
